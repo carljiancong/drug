e4c2b7fb1bc3f8ed77ab22a91b775566e5f007e9
dongqi <dongqi@harmonycloud.cn>
2019-03-15 16:12:36 +0800

    update drug

--- src/main/java/com/harmonycloud/main/DrugApplication.java
+++ src/main/java/com/harmonycloud/main/DrugApplication.java
@@ -3,6 +3,8 @@ package com.harmonycloud.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
@@ -12,10 +14,13 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @ComponentScan("com.harmonycloud")
 @EnableMongoRepositories("com.harmonycloud.monRepository")
-@EnableJpaRepositories("com.harmonycloud.oraRepository")
 @EntityScan("com.harmonycloud.entity")
-public class DrugApplication {
+public class DrugApplication extends SpringBootServletInitializer {
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
+    }
     public static void main(String[] args) {
         SpringApplication.run(DrugApplication.class, args);
     }
